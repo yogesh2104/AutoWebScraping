507c492db14cae8b956ac50f38781f161176719a
iamyogesh21 <singhyogesh2104>
2021-06-17 02:11:36 +0530

    initial commit.

--- scrapper.py
+++ scrapper.py
@@ -6,15 +6,26 @@ from autoscraper import AutoScraper
 # paste the link which you want to search on amazon or flipkart etc link should be the string 
 # format
 
-url = "https://www.amazon.in/s?k=laptop"
-''' here i am searching laptop on amazon
+url = "https://www.amazon.in/s?k=iphone"
+''' here i am searching iphone on amazon
     after the link make a list what you want to scrape
     like price, rating, discount etc.
 '''
-wanted_list = ["ASUS VivoBook 14 (2020) Intel Quad Core Pentium Silver N5030, 14-inch (35.56 cms) FHD Thin and Light Laptop (4GB RAM/1TB HDD/Windows 10/Integrated Graphics/Transparent Silver/1.5 Kg), X415MA-EK101T","185","24,990","7,000"]
+wanted_list = ["New Apple iPhone 12 (128GB) - Blue","3,119","4,670","80,230"]
 # in wnated list you can select any one product and grouped them into list
 
-# scraper = AutoScraper()
+scraper = AutoScraper()
+
 # result = scraper.build(url, wanted_list)
 # print(result)
 
+# print(help(scraper.get_result_similar))
+
+scraper.set_rule_aliases({'rule_wnb2':"Title"})
+scraper.keep_rules(['rule_wnb2'])
+scraper.save('amazon-search')
+
+
+result = scraper.get_result_similar("https://www.amazon.in/s?k=realme+phones&rh=n%3A1389432031%2Cp_89%3Arealme&dc&qid=1623875809&rnid=3837712031&ref=sr_nr_p_89_1",group_by_alias=True)
+result['Title']
+print(result)
